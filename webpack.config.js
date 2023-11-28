7e00cc99961fa8e28025b018bd9dbd163ce7d703
choi-ik <ci980704@naver.com>
2023-11-28 16:40:15 +0900

    Add router

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
   entry: "./src/main.js",
   output: {
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
     clean: true,
     // filename: "",
   },
@@ -41,4 +42,7 @@ module.exports = {
       ],
     }),
   ],
+  devServerL: {
+    historyApiFallback: true,
+  },
 };
